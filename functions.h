3c563ddd7cbbcce4274ec6d8a4557fd595d3b383
ray33ee <30669752+ray33ee@users.noreply.github.com>
2019-01-16 23:06:12 +0000

    Version 1.0.2 - Functions added and setup/loop control flow implemented
    Signed-off-by: ray33ee <30669752+ray33ee@users.noreply.github.com>

--- functions.h
+++ functions.h
@@ -1,5 +1,9 @@
+#ifndef VMINTERPRETER_FUNCTIONS
+#define VMINTERPRETER_FUNCTIONS
 
-#include <stdio.h>
+#include <stdio.h> //printf
+#include <cstdlib> //std::rand
+#include <math.h>
 
 class Flags
 {
@@ -10,69 +14,321 @@ public:
 	Flags(bool s, bool e) : sign(s), equal(e) {}
 } ;
 
+//Dirty cast from int to float
 float hardConvert(int num)
 {
 	return *(float*)&num;
 }
 
+//Dirty cast from float back to int
 int hardConvert(float num)
 {
 	return *(int*)&num;
 }
 
-void instr_nop()
+void instr_nop() //0x0000
 {
 	
 }
 
-void instr_halt()
-{
-	while(1);
-}
-
-void instr_ret()
+void instr_ret() //0x0001
 {
 	
 }
 
-void instr_dec_vi(int* args, int* reg, Flags* flags) //107
+void instr_dec_vi(int* args, int* reg, Flags* flags) //0x0107
 {
 	--reg[args[0]];
 	*flags = Flags( reg[args[0]] < 0, reg[args[0]] == 0 );
 }
 
-void instr_store_vi_li(int* args, int* reg) //246
+void instr_inc_vi(int* args, int* reg, Flags* flags) //0x0108
 {
-	reg[args[0]] = args[1];
-	printf("    val: %i\n", args[1]);
+	++reg[args[0]];
+	*flags = Flags( reg[args[0]] < 0, reg[args[0]] == 0 );
 }
 
-void instr_store_vf_lf(int* args, int* reg) //247
+void instr_random_vi(int* args, int* reg) //0x0109
 {
-	reg[args[0]] = args[1];
-	printf("    STORE: %i\n", (args[0]));
+	reg[args[0]] = std::rand();
 }
 
-void instr_add_vi_vi_vi(int* args, int* reg, Flags* flags) //300
+void instr_random_vf(int* args, int* reg) //0x010A
+{
+	reg[args[0]] = hardConvert((float)std::rand() / RAND_MAX);
+}
+
+void instr_random_vv(int* args, int* reg) //0x010B
+{
+	
+}
+
+//Arguments 0x010C to 0x010F are stack arguments. Kinda need to figure out what ima do with the stack first...
+
+//Not really sure if this is needed...
+void instr_start_vi(int* args, int* reg) //0x0110
+{
+	
+}
+
+void instr_test_vi(int* args, int* reg, Flags* flags) //0x0111
+{
+	*flags = Flags(false, reg[args[0]] == 0);
+}
+
+void instr_test_vf(int* args, int* reg, Flags* flags) //0x0112
+{
+	*flags = Flags(false, hardConvert(reg[args[0]]) == 0);
+}
+
+void instr_test_vv(int* args, int* reg, Flags* flags) //0x0113
+{
+	
+}
+
+void instr_sin_vf_vf(int* args, int* reg) //0x0200
+{
+	reg[args[0]] = hardConvert(sinf(hardConvert(reg[args[1]])));
+}
+
+void instr_cos_vf_vf(int* args, int* reg) //0x0201
+{
+	reg[args[0]] = hardConvert(cosf(hardConvert(reg[args[1]])));
+}
+
+void instr_tan_vf_vf(int* args, int* reg) //0x0202
+{
+	reg[args[0]] = hardConvert(tanf(hardConvert(reg[args[1]])));
+}
+
+void instr_abs_vi_vi(int* args, int* reg) //0x0203
+{
+	reg[args[0]] = abs(reg[args[1]]);
+}
+
+void instr_abs_vf_vf(int* args, int* reg) //0x0204
+{
+	reg[args[0]] = hardConvert(abs(hardConvert(reg[args[1]])));
+}
+
+void instr_norm_vi_vi(int* args, int* reg) //0x0205
+{
+	reg[args[0]] = signbit(reg[args[1]]) ? -1 : 1;
+}
+
+void instr_norm_vf_vf(int* args, int* reg) //0x0206
+{
+	reg[args[0]] = hardConvert(signbit(hardConvert(reg[args[1]])) ? -1.0f : 1.0f);
+}
+
+void instr_ln_vf_vf(int* args, int* reg) //0x0207
+{
+	reg[args[0]] = hardConvert(logf(hardConvert(reg[args[1]])));
+}
+
+void instr_exp_vf_vf(int* args, int* reg) //0x0208
+{
+	reg[args[0]] = hardConvert(expf(hardConvert(reg[args[1]])));
+}
+
+void instr_floor_vf_vf(int* args, int* reg) //0x0209
+{
+	reg[args[0]] = hardConvert(floorf(hardConvert(reg[args[1]])));
+}
+
+void instr_ceil_vf_vf(int* args, int* reg) //0x020A
+{
+	reg[args[0]] = hardConvert(ceilf(hardConvert(reg[args[1]])));
+}
+
+void instr_sqrt_vf_vf(int* args, int* reg) //0x020B
+{
+	reg[args[0]] = hardConvert(sqrtf(hardConvert(reg[args[1]])));
+}
+
+void instr_neg_vf_vf(int* args, int* reg) //0x020C
+{
+	reg[args[0]] = hardConvert(-hardConvert(reg[args[1]]));
+}
+
+void instr_neg_vi_vi(int* args, int* reg) //0x020D
+{
+	reg[args[0]] = -reg[args[1]];
+}
+
+void instr_store_vi_li(int* args, int* reg) //0x0240
+{
+	reg[args[0]] = args[1];
+}
+
+void instr_store_vf_lf(int* args, int* reg) //0x0241
+{
+	reg[args[0]] = args[1];
+}
+
+void instr_store_vi_vi(int* args, int* reg) //0x0242
+{
+	reg[args[0]] = reg[args[1]];
+}
+
+void instr_store_vi_vf(int* args, int* reg) //0x0243
+{
+	reg[args[0]] = (int)hardConvert(reg[args[1]]);
+}
+
+void instr_store_vf_vi(int* args, int* reg) //0x0244
+{
+	reg[args[0]] = hardConvert((float)reg[args[1]]);
+}
+
+void instr_store_vf_vf(int* args, int* reg) //0x0245
+{
+	reg[args[0]] = reg[args[1]];
+}
+
+void instr_cmp_vi_li(int* args, int* reg, Flags* flags) //0x0246
+{
+	*flags = Flags(reg[args[0]] < args[1], reg[args[0]] == args[1]);
+}
+
+void instr_cmp_vf_lf(int* args, int* reg, Flags* flags) //0x0247
+{
+	*flags = Flags(hardConvert(reg[args[0]]) < hardConvert(args[1]), 
+				   hardConvert(reg[args[0]]) == hardConvert(args[1]));
+}
+
+void instr_cmp_vi_vi(int* args, int* reg, Flags* flags) //0x0248
+{
+	*flags = Flags(reg[args[0]] < reg[args[1]], 
+				   reg[args[0]] == reg[args[1]]);
+}
+
+void instr_cmp_vi_vf(int* args, int* reg, Flags* flags) //0x0249
+{
+	*flags = Flags(reg[args[0]] < hardConvert(reg[args[1]]), 
+				   reg[args[0]] == hardConvert(reg[args[1]]));
+}
+
+void instr_cmp_vf_vi(int* args, int* reg, Flags* flags) //0x024A
+{
+	*flags = Flags(hardConvert(reg[args[0]]) < reg[args[1]], 
+				   hardConvert(reg[args[0]]) == reg[args[1]]);
+}
+
+void instr_cmp_vf_vf(int* args, int* reg, Flags* flags) //0x024B
+{
+	*flags = Flags(hardConvert(reg[args[0]]) < hardConvert(reg[args[1]]), 
+				   hardConvert(reg[args[0]]) == hardConvert(reg[args[1]]));
+}
+
+//Like start, again, not sure if this is needed
+void instr_stop_vi_vi(int* args, int* reg) //0x024C
+{
+	
+}
+
+void instr_add_vi_vi_vi(int* args, int* reg, Flags* flags) //0x0300
 {
 	reg[args[0]] = reg[args[1]] + reg[args[2]];
 	*flags = Flags( reg[args[0]] < 0, reg[args[0]] == 0 );
 }
 
-void instr_add_vf_vf_vf(int* args, int* reg, Flags* flags) //301
+void instr_add_vf_vf_vf(int* args, int* reg, Flags* flags) //0x0301
 {
 	reg[args[0]] = hardConvert(hardConvert(reg[args[1]]) + hardConvert(reg[args[2]]));
 	*flags = Flags( hardConvert(reg[args[0]]) < 0, hardConvert(reg[args[0]]) == 0 );
 }
 
-void instr_mul_vi_vi_li(int* args, int* reg, Flags* flags) //310
+void instr_add_vv_vv_vv(int* args, int* reg, Flags* flags) //0x0302
+{
+	
+}
+
+void instr_add_vi_vi_li(int* args, int* reg, Flags* flags) //0x0303
+{
+	reg[args[0]] = reg[args[1]] + args[2];
+	*flags = Flags( reg[args[0]] < 0, args[0] == 0 );
+}
+
+void instr_add_vf_vf_lf(int* args, int* reg, Flags* flags) //0x0304
+{
+	reg[args[0]] = hardConvert(hardConvert(reg[args[1]]) + hardConvert(args[2]));
+	*flags = Flags( hardConvert(reg[args[0]]) < 0, hardConvert(args[0]) == 0 );
+}
+
+void instr_sub_vi_vi_vi(int* args, int* reg, Flags* flags) //0x0305
+{
+	reg[args[0]] = reg[args[1]] - reg[args[2]];
+	*flags = Flags( reg[args[0]] < 0, reg[args[0]] == 0 );
+}
+
+void instr_sub_vf_vf_vf(int* args, int* reg, Flags* flags) //0x0306
+{
+	reg[args[0]] = hardConvert(hardConvert(reg[args[1]]) - hardConvert(reg[args[2]]));
+	*flags = Flags( hardConvert(reg[args[0]]) < 0, hardConvert(reg[args[0]]) == 0 );
+}
+
+void instr_sub_vv_vv_vv(int* args, int* reg, Flags* flags) //0x0307
+{
+	
+}
+
+void instr_sub_vi_vi_li(int* args, int* reg, Flags* flags) //0x0308
+{
+	reg[args[0]] = reg[args[1]] - args[2];
+	*flags = Flags( reg[args[0]] < 0, args[0] == 0 );
+}
+
+void instr_sub_vf_vf_lf(int* args, int* reg, Flags* flags) //0x0309
+{
+	reg[args[0]] = hardConvert(hardConvert(reg[args[1]]) - hardConvert(args[2]));
+	*flags = Flags( hardConvert(reg[args[0]]) < 0, hardConvert(args[0]) == 0 );
+}
+
+void instr_sub_vi_li_vi(int* args, int* reg, Flags* flags) //0x030A
+{
+	reg[args[0]] = args[1] - reg[args[2]];
+	*flags = Flags( reg[args[0]] < 0, args[0] == 0 );
+}
+
+void instr_sub_vf_lf_vf(int* args, int* reg, Flags* flags) //0x030B
+{
+	reg[args[0]] = hardConvert(hardConvert(args[1]) - hardConvert(reg[args[2]]));
+	*flags = Flags( hardConvert(reg[args[0]]) < 0, hardConvert(args[0]) == 0 );
+}
+
+void instr_mul_vi_vi_vi(int* args, int* reg, Flags* flags) //0x030C
+{
+	reg[args[0]] = reg[args[1]] * reg[args[2]];
+	*flags = Flags( reg[args[0]] < 0, reg[args[0]] == 0 );
+}
+
+void instr_mul_vf_vf_vf(int* args, int* reg, Flags* flags) //0x030D
+{
+	reg[args[0]] = hardConvert(hardConvert(reg[args[1]]) * hardConvert(reg[args[2]]));
+	*flags = Flags( hardConvert(reg[args[0]]) < 0, hardConvert(reg[args[0]]) == 0 );
+}
+
+void instr_mul_vv_vv_vf(int* args, int* reg, Flags* flags) //0x030E
+{
+	
+}
+
+void instr_mul_vv_vv_lf(int* args, int* reg, Flags* flags) //0x030F
+{
+	
+}
+
+void instr_mul_vi_vi_li(int* args, int* reg, Flags* flags) //0x0310
 {
 	reg[args[0]] = reg[args[1]] * args[2];
 	*flags = Flags( reg[args[0]] < 0, reg[args[0]] == 0 );
 }
 
-void instr_mul_vf_vf_lf(int* args, int* reg, Flags* flags) //311
+void instr_mul_vf_vf_lf(int* args, int* reg, Flags* flags) //0x0311
 {
 	reg[args[0]] = hardConvert(hardConvert(reg[args[1]]) * hardConvert(args[2]));
 	*flags = Flags( hardConvert(reg[args[0]]) < 0, hardConvert(reg[args[0]]) == 0 );
 }
+
+#endif
--- main.cxx
+++ main.cxx
@@ -22,11 +22,13 @@
  */
 
 
-#include <iostream>
-#include <stdio.h>
-#include "functions.h"
+#include <iostream> //cout
+#include <fstream> //ifstream
+#include <stdio.h> //printf
+#include <cstdlib> //std::rand
+#include <ctime> //std::time
 
-#include <fstream>
+#include "functions.h"
 
 const int REGISTER_COUNT = 30;
 
@@ -34,16 +36,12 @@ const int REGISTER_COUNT = 30;
 int start_machine(std::ifstream* file, int* registers, Flags* flags)
 {
 	using namespace std;
-	//Allocate registers
-	//int registers[REGISTER_COUNT];
-	
-	//Flags flags(false, false);
-	
-	//int timeout = 1000;
 	
 	//Get the op code pointed to by the program counter (PC)
-	while (!file->eof())
+	while (true)
 	{
+		
+		
 		//Current bytecode
 		int fetch;
 		
@@ -53,7 +51,13 @@ int start_machine(std::ifstream* file, int* registers, Flags* flags)
 		//Fetch opcode
 		file->read((char*)&fetch, sizeof(fetch));
 		
-		printf("code: %x\n", fetch);
+		if (file->eof())
+			break;
+		
+		printf("code %x at %i\n", fetch, (int)file->tellg());
+		
+		printf("    flags: %i %i %i %i\n", file->good(), file->eof(), file->fail(), file->bad());
+		
 		
 		//Calculate number of arguments for this opcode
 		int argcount = (fetch / 256);
@@ -71,28 +75,197 @@ int start_machine(std::ifstream* file, int* registers, Flags* flags)
 		//Decode and execute instruction
 		switch (fetch)
 		{
+			//Zero argument functions
 			case 0x0:
 				instr_nop();
 				break;
-			case 0x0102:
+			
+			//Single argument functions
+			case 0x0100: //jmp_li
+				file->seekg(args[0] * sizeof(int)); //Seek to jump location
+				break;
+			case 0x0101: //je_li
+				if (flags->equal)
+					file->seekg(args[0] * sizeof(int)); //Seek to jump location
+				break;
+			case 0x0102: //jne_li
 				if (!flags->equal)
 					file->seekg(args[0] * sizeof(int)); //Seek to jump location
+				break;
+			case 0x0103: //jl_li
+				if (flags->sign)
+					file->seekg(args[0] * sizeof(int)); //Seek to jump location
+				break;
+			case 0x0104: //jle_li
+				if (!flags->sign)
+					file->seekg(args[0] * sizeof(int)); //Seek to jump location
+				break;
+			case 0x0105: //external_li
+				
+				break;
+			case 0x0106: //elapsed_vi
+				
 				break;
 			case 0x0107:
 				instr_dec_vi(args, registers, flags);
 				break;
-			case 0x0246:
+			case 0x0108:
+				instr_inc_vi(args, registers, flags);
+				break;
+			case 0x0109:
+				instr_random_vi(args, registers);
+				break;
+			case 0x010A:
+				instr_random_vf(args, registers);
+				break;
+			case 0x010B:
+				instr_random_vv(args, registers);
+				break;
+			case 0x0111:
+				instr_test_vi(args, registers, flags);
+				break;
+			case 0x0112:
+				instr_test_vf(args, registers, flags);
+				break;
+			case 0x0113:
+				instr_test_vv(args, registers, flags);
+				break;
+				
+				
+			case 0x0200:
+				instr_sin_vf_vf(args, registers);
+				break;
+			case 0x0201:
+				instr_cos_vf_vf(args, registers);
+				break;
+			case 0x0202:
+				instr_tan_vf_vf(args, registers);
+				break;
+			case 0x0203:
+				instr_abs_vi_vi(args, registers);
+				break;
+			case 0x0204:
+				instr_abs_vf_vf(args, registers);
+				break;
+			case 0x0205:
+				instr_norm_vf_vf(args, registers);
+				break;
+			case 0x0206:
+				instr_norm_vf_vf(args, registers);
+				break;
+			case 0x0207:
+				instr_ln_vf_vf(args, registers);
+				break;
+			case 0x0208:
+				instr_exp_vf_vf(args, registers);
+				break;
+			case 0x0209:
+				instr_floor_vf_vf(args, registers);
+				break;
+			case 0x020A:
+				instr_ceil_vf_vf(args, registers);
+				break;
+			case 0x020B:
+				instr_sqrt_vf_vf(args, registers);
+				break;
+			case 0x020C:
+				instr_neg_vi_vi(args, registers);
+				break;
+			case 0x020D:
+				instr_neg_vf_vf(args, registers);
+				break;
+				
+				
+			case 0x0240:
 				instr_store_vi_li(args, registers);
 				break;
-			case 0x0247:
+			case 0x0241:
 				instr_store_vf_lf(args, registers);
 				break;
+			case 0x0242:
+				instr_store_vi_vi(args, registers);
+				break;
+			case 0x0243:
+				instr_store_vi_vf(args, registers);
+				break;
+			case 0x0244:
+				instr_store_vf_vi(args, registers);
+				break;
+			case 0x0245:
+				instr_store_vf_vf(args, registers);
+				break;
+				
+			case 0x0246:
+				instr_cmp_vi_li(args, registers, flags);
+				break;
+			case 0x0247:
+				instr_cmp_vf_lf(args, registers, flags);
+				break;
+			case 0x0248:
+				instr_cmp_vi_vi(args, registers, flags);
+				break;
+			case 0x0249:
+				instr_cmp_vi_vf(args, registers, flags);
+				break;
+			case 0x024A:
+				instr_cmp_vf_vi(args, registers, flags);
+				break;
+			case 0x024B:
+				instr_cmp_vf_vf(args, registers, flags);
+				break;
+			
+				
 			case 0x0300:
 				instr_add_vi_vi_vi(args, registers, flags);
 				break;
 			case 0x0301:
 				instr_add_vf_vf_vf(args, registers, flags);
 				break;
+			case 0x0302:
+				instr_add_vv_vv_vv(args, registers, flags);
+				break;
+			case 0x0303:
+				instr_add_vi_vi_li(args, registers, flags);
+				break;
+			case 0x0304:
+				instr_add_vf_vf_lf(args, registers, flags);
+				break;
+				
+			case 0x0305:
+				instr_sub_vi_vi_vi(args, registers, flags);
+				break;
+			case 0x0306:
+				instr_sub_vf_vf_vf(args, registers, flags);
+				break;
+			case 0x0307:
+				instr_sub_vv_vv_vv(args, registers, flags);
+				break;
+			case 0x0308:
+				instr_sub_vi_vi_li(args, registers, flags);
+				break;
+			case 0x0309:
+				instr_sub_vf_vf_lf(args, registers, flags);
+				break;
+			case 0x030A:
+				instr_sub_vi_li_vi(args, registers, flags);
+				break;
+			case 0x030B:
+				instr_sub_vf_lf_vf(args, registers, flags);
+				break;
+				
+			case 0x030C:
+				instr_mul_vi_vi_vi(args, registers, flags);
+				break;
+			case 0x030D:
+				instr_mul_vf_vf_vf(args, registers, flags);
+				break;
+			case 0x030E:
+				instr_mul_vv_vv_vf(args, registers, flags);
+				break;
+			case 0x030F:
+				instr_mul_vv_vv_lf(args, registers, flags);
+				break;
+				
 			case 0x0310:
 				instr_mul_vi_vi_li(args, registers, flags);
 				break;
@@ -100,63 +273,84 @@ int start_machine(std::ifstream* file, int* registers, Flags* flags)
 				instr_mul_vf_vf_lf(args, registers, flags);
 				break;
 				
+			default:
+				printf("Error: Bytecode 0x%08X not recognised. Aborting VM\n", fetch);
+				return 1;
+				
 		}
 		
-		std::cout << "    registers: ";
+		cout << "    registers: ";
 		
-		for (int i = 0; i < 5; ++i)
+		for (int i = 0; i < 4; ++i)
 			printf("%i ", registers[i]);
 			
-		printf("%f\n", hardConvert(registers[5]));
+		printf("%g\n", hardConvert(registers[4]));
 			
 	}
 	
-	//printf("Program counter: %i\n", program_counter);
+	//Clear eof and error flags
+	file->clear();
 	
-	
-	//Execute the instruction, referencing first argument by PC+1, second by PC+2, etc.
-	//Incremement Program counter accordingly
+	//Reset stream 
+	file->seekg(0);
 	
 	return 0;
 }
 
+void validator()
+{
+	int args[20];
+	int reg[30];
+	Flags flags(false, false);
+	
+	//Use resister 0 for first arg
+	args[0] = 0;
+	args[1] = 1;
+	
+	reg[1] = hardConvert(1.33f);
+	reg[2] = 0;
+	
+	instr_store_vf_vf(args, reg);
+	
+	printf("Integer: %i\n", reg[0]);
+	printf("Float  : %g\n", hardConvert(reg[0]));
+	
+}
+
+
 int main(int argc, char **argv)
 {
 	using namespace std;
-	
-	char* path = argv[1];
+	char* setup_path = argv[1];
+	char* loop_path = argv[2];
 	int registers[30];
-	ifstream* bytecodes;
+	ifstream setup(setup_path, ifstream::binary);
+	ifstream loop(loop_path, ifstream::binary);
+	
+	srand(time(nullptr));
 	
 	Flags flags(false, false);
 	
 	//Get the file path from command line args
-	if (argc != 2)
+	if (argc != 3)
 	{
-		cout << "Error: Invalid number of command line arguments." << endl;
+		cout << "Error: Invalid number of command line arguments. Aborting VM." << endl;
 		return 1;
 	}
 	
-	//Load contents into memory
-	//file = fopen(path, "rb");
-	//codecount = getFileSize(file) / sizeof(*bytecodes);
-	//bytecodes = new int[codecount];
+	cout << "START SETUP ROUTINE" << endl;
+	if (start_machine(&setup, registers, &flags))
+		return 1;
+		
+	cout << "START LOOP ROUTING" << endl;
 	
-	bytecodes = new ifstream(path, ifstream::binary);
-	
-	
-	//fread(bytecodes, sizeof(*bytecodes), codecount, file);
-	
-	/*codecount = 1;
-	bytecodes = new int[codecount];
-	
-	bytecodes[0] = 0x246;
-	bytecodes[1] = 0x4;
-	bytecodes[2] = 0x64;*/
+	for (int i = 0; i < 10; ++i)
+	{
+		if (start_machine(&loop, registers, &flags))
+			return 1;
+	}
 	
 	
-	
-	start_machine(bytecodes, registers, &flags);
 	
 	return 0;
 }
